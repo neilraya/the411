ebb9f82f311613a99ca7d865a5bcbec597287bac
neilraya <neil.raya1@live.com>
2019-10-25 10:17:20 -0400

    created forms.py

--- prototype/prototype/weather/views.py
+++ prototype/prototype/weather/views.py
@@ -7,6 +7,8 @@ def weather(request):
 
     url = "http://samples.openweathermap.org/data/2.5/weather"
 
+    form = searchForm()
+
     querystring = {"q": "London,uk","appid":"b6907d289e10d714a6e88b30761fae22", "units": "imperial"}
 
     #querystring['q'] = request.POST
