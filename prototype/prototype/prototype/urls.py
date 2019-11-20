f43c71f2d0c58939d19e752896c6e39d95562694
Kathryn-Hackman <hackmank@bu.edu>
2019-11-20 15:06:17 -0500

    take out key

--- prototype/prototype/prototype/urls.py
+++ prototype/prototype/prototype/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('weather.urls')),
     path('admin/', admin.site.urls),
-    url(r'^accounts/',include('allauth.urls')),
+    path(r'^accounts/',include('allauth.urls')),
 ]
